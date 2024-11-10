320ae8bd34bd66c4f7af49047259da90fd5cbf9a
kkoouuya0730 <kouya.0730@icloud.com>
2024-11-10 16:34:43 +0900

    Create Icons

---
+++ src/components/ui/icons/UseIon.tsx
@@ -0,0 +1,25 @@
+import { AcademicIcon } from "./AcademicIcon";
+import { AddressIcon } from "./AddressIcon";
+import { BrainIcon } from "./BrainIcon";
+import { EmailIcon } from "./EmailIcon";
+import { GithubIcon } from "./GithubIcon";
+import { PhoneIcon } from "./PhoneIcon";
+
+const iconsMapping = {
+  academic: AcademicIcon,
+  address: AddressIcon,
+  brain: BrainIcon,
+  email: EmailIcon,
+  github: GithubIcon,
+  phone: PhoneIcon,
+};
+
+export type IconName = keyof typeof iconsMapping;
+
+type Props = {
+  iconName: IconName;
+};
+export default function UseIon({ iconName }: Props) {
+  const SelectedIcon = iconsMapping[iconName];
+  return <SelectedIcon />;
+}
