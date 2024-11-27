54fe9b1591c8d46a1214dad777f447efa814878a
kkoouuya0730 <kouya.0730@icloud.com>
2024-11-27 20:46:42 +0900

    Old main

---
+++ out/_next/static/chunks/app/layout-938eb7adbf6d572a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{9971:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,7970,23)),Promise.resolve().then(n.t.bind(n,4839,23)),Promise.resolve().then(n.t.bind(n,347,23))},347:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[690,74,441,517,358],()=>s(9971)),_N_E=e.O()}]);
