f6edcd4823200a4864522ada5e5b17092e4bd359
kkoouuya0730 <kouya.0730@icloud.com>
2024-11-29 00:51:20 +0900

    clsxとmarkuplintを追加

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["avatars.githubusercontent.com"], // 外部ドメインを追加
+    unoptimized: true, // 最適化を無効 静的エクスポートと相入れない
   },
   output: "export",
 };
